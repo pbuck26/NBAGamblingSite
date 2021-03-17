6097467a4a99992148524402d8aba6d093da4428
pbuck26 <patrickbuckley@patricks-air.lan>
2021-03-16 22:21:21 -0400

    Working Copy

--- app.py
+++ app.py
@@ -1,28 +1,36 @@
 from flask import Flask, request, render_template, jsonify
-from service import NewUserService
-from model import Schema, UserModel
 import json
+import sklearn
+from sklearn.naive_bayes import GaussianNB
+import pandas as pd
+import numpy as np
+import os
+from sportsreference.nba.schedule import Schedule
+from sportsreference.nba.boxscore import Boxscores, Boxscore
+from sportsreference.nba.teams import Teams
+from datetime import date, timedelta
+import requests
+import scrapeTodaysGames as sc
+import trainAndExportModel as tm
 
 app = Flask(__name__)
 
-@app.after_request
-def add_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] =  "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-    response.headers['Access-Control-Allow-Methods']=  "POST, GET, PUT, DELETE, OPTIONS"
-    return response
+#Daily function calls
+# Will have to see if I can scrape data using webdriver on server
+Model  = tm.trainAndExportModel()
+games = sc.scrapeGamesAndOdds(Model)
+
+
+@app.route("/")
+def renderHomepage():
+    # have to add a "pick" variable
+    # verify that the path to static png file works
+    # arrange picks into one variable
+    # move website html to index file
+    
+    return render_template('websitetake2.html', games = games)
 
 #define route
-@app.route("/")
-def hello():
-    return render_template('index.html', name = 'Pat')
-
-@app.route("/newuser", methods=["POST"])
-def new_user():
-    try:
-        return NewUserService().createNewUser(request.get_json())
-    except:
-        print(NewUserService().createNewUser(request.get_json()))
-if __name__ == "__main__":
-    Schema()
-    app.run(debug = True)
+@app.route("/result',methods = ['POST']")
+def hello(temp):
+    return
--- service.py
+++
@@ -1,7 +0,0 @@
-from model import UserModel, Schema
-class NewUserService:
-    def __init__(self):
-        self.model = UserModel()
-
-    def createNewUser(self, params):
-        return self.model.create(params)
