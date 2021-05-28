4bb6baab87657dd593fda53fd3cbc0a08a7127ba
pbuck26 <patrickbuckley@Patricks-Air.localdomain>
2021-05-28 14:21:43 -0400

    poopy

--- patSite/__init__.py
+++ patSite/__init__.py
@@ -25,27 +25,5 @@ def create_app(test_config=None):
         model = pickle.load(open('model.pkl','rb'))
         global games
         games = scrapeGamesAndOdds(model)
-
-    @app.route("/")
-    def renderHomepage():
-        app.logger.info('homepage')
-        # have to add a "pick" variable
-        # verify that the path to static png file works
-        # arrange picks into one variable
-        # move website html to index file
-        return render_template('websitetake2.html', games = games, user_verified = False)
-    
-    @app.route("/", methods=['POST', 'GET'])
-    def get_email():
-        app.logger.info('Email request endpoint')
-        if "email" in request.form:
-            email = request.form['email']
-            password = request.form['pwd']
-            data = Users(email, password)
-            db.session.add(data)
-            db.session.commit()
-            return render_template('websitetake2.html', games = games, user_verified = True)
-        else:
-            app.logger.error('poopoo')
-
+        
     return app
---
+++ patSite/routes.py
@@ -0,0 +1,25 @@
+from flask import current_app as app
+from flask import request, render_template, jsonify, Response
+from .db import Users, db
+
+@app.route("/")
+def renderHomepage():
+    app.logger.info('homepage')
+    # have to add a "pick" variable
+    # verify that the path to static png file works
+    # arrange picks into one variable
+    # move website html to index file
+    return render_template('websitetake2.html', games = games, user_verified = False)
+
+@app.route("/", methods=['POST', 'GET'])
+def get_email():
+    app.logger.info('Email request endpoint')
+    if "email" in request.form:
+        email = request.form['email']
+        password = request.form['pwd']
+        data = Users(email, password)
+        db.session.add(data)
+        db.session.commit()
+        return render_template('websitetake2.html', games = games, user_verified = True)
+    else:
+        app.logger.error('poopoo')
