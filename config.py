b5c77e004d0fa8ca68a6e7a0bd6a151927141de8
pbuck26 <patrickbuckley@Patricks-Air.localdomain>
2021-05-28 13:39:56 -0400

    poopy

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ from os import environ, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))
+load_dotenv(path.join(basedir, '.env'))
 
 
 class Config:
     DEBUG = False
     DEVELOPMENT = False
-    SECRET_KEY = environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    FLASK_APP =environ.get("FLASK_APP")
-    FLASK_ENV=environ.get("FLASK_ENV")
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    FLASK_APP =environ.get('FLASK_APP')
+    FLASK_ENV=environ.get('FLASK_ENV')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
