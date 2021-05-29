483efad1f52351f3f95b852012b317a7f35de98c
pbuck26 <patrickbuckley@Patricks-Air.localdomain>
2021-05-29 17:15:39 -0400

    poopy

--- patSite/forms.py
+++ patSite/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional, email_validator
 
 class SignupForm(FlaskForm):
     name = StringField(
