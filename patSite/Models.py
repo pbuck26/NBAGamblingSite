9481eda4d67847281cdae750f855a64ad05088bb
pbuck26 <patrickbuckley@patricks-air.lan>
2021-05-31 23:03:13 -0400

    poopy

--- patSite/Models.py
+++ patSite/Models.py
@@ -25,7 +25,7 @@ class Users(UserMixin, db.Model):
         self.password = generate_password_hash(password, 'sha256')
 
     def check_password(self, password):
-        return check_password_hash(self.password)
+        return check_password_hash(self.password, password)
     
     def __repr__(self):
         return '<User {}>'.format(self.id)
